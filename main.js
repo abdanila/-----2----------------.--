daf56c3f9da6df4f141a0520301193690bf8f2af
abdanila <absatarov7405@mail.ru>
2024-12-16 13:28:14 +0300

    js-custom

--- main.js
+++ main.js
@@ -6,10 +6,13 @@ const imtInfo = document.querySelector('#imt-info');
 const imtNone = document.querySelector('.imtNone');
 const imtNoneRes = document.querySelector('.imtNone-res');
 const imtResult = document.querySelector('.imt__info-result');
+
+
 function validation(){
     let inputValueHeight = +inputInfo[0].value;
     let inputValueWeight = +inputInfo[1].value;
-    function imt(height, weight){    
+
+function imt(height, weight){    
         let result = weight / Math.pow((height/100), 2);
         if(isNaN(inputValueHeight) || isNaN(inputValueWeight)){alertInfo.classList.remove("none");imtNone.classList.add('none')}
         else if((inputValueHeight==0 || inputValueWeight==0)){console.log("Ошибка, поля ввода пустые");numInfo.classList.remove("none");imtNone.classList.add('none')}
@@ -17,10 +20,10 @@ function validation(){
         if(result>=18.5){imtResult.textContent = "Норма";imtNoneRes.classList.remove('none')}
         else if(result<18.5){imtResult.textContent = "Недостаточная(дефицит) масса тела";imtNoneRes.classList.remove('none');imtResult.style.color = "#000"}
         else if(result>=25){imtResult.textContent = "Избыточная масса тела(предожирение)";imtNoneRes.classList.remove('none');imtResult.style.color = "#000"}
-    }
+}
         imt(inputValueHeight, inputValueWeight)
         
-    }
-    btnSubmit.addEventListener("click", function(){validation()})
+}
+        btnSubmit.addEventListener("click", function(){validation()})
 
     
